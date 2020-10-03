c6f8e8097b23424cc0ca76b993a0341de1cae0a0
Surya Prakash Gupta <sugamprakash@gmail.com>
2020-10-03 13:54:18 +0530

    This is the Class implemetation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pyclass
-This is a simple class implementation in python
+This is a simple class implementation in python. The easiest way to remember class.
+Thing to remember is self is nothing but the object name.
+
+If there is self.name means its object.name
--- part1.py
+++ part1.py
@@ -1,1 +1,18 @@
 print("Hello class")
+
+class Robot:
+    """This class will return a object which will cointain name colour of  the robot"""
+    """This is a simple class implementation in python. The easiest way to remember class.
+Thing to remember is self is nothing but the object name."""
+    #Defining Constructors 
+
+    def __init__ (self,name,colour):
+        self.name = name
+        self.colour=colour
+
+    def hello(self):
+        print ("Hello! My name is "+self.name+" and colour is " + self.colour)
+
+
+r= Robot("Surya","Red")
+r.hello()
